6b29c1c0c43936ee175280cb2be5de4bb35ed718
Hans-Bums <thomasfaltins@arcor.de>
2026-05-07 22:15:16 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+// SaDro Service Worker – v024
+const CACHE = 'sadro-v024';
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(['/SaDro/']))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Network-first: immer neue Version laden, Fallback auf Cache
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
